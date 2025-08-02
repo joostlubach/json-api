273a93d1455afb7e019ff2a4cad9a67b3565ea58
Joost Lubach <joostlubach@gmail.com>
2025-08-02 11:19:11 +0200

    Fix

--- src/RequestContext.ts
+++ src/RequestContext.ts
@@ -26,8 +26,8 @@ export default class RequestContext<P extends Record<string, any> = Record<strin
    * @param name The name of the parameter to retrieve.
    * @param type Optionally a type to validate against.
    */
-  public param<Output, Def extends z.ZodTypeDef = z.ZodTypeDef, Input = Output>(name: string & keyof P, schema: z.ZodSchema<Output, Def, Input>): Output
-  public param(name: string & keyof P, schema: z.ZodSchema) {
+  public param<Output, Def extends z.ZodTypeDef = z.ZodTypeDef, Input = Output>(name: string & keyof P, schema: z.Schema<Output, Def, Input>): Output
+  public param(name: string & keyof P, schema: z.Schema) {
     let value = this.params[name]
     if (schema == null) { return value }
 
